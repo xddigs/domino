c4e59b71142b8af4c84ffd54a0f53d724b3bff4e
diego <sargent.pointed453@passinbox.com>
2026-05-05 13:04:33 +0200

    Update cleaned up the main class

--- Domino.Core/DominoGame.cs
+++ Domino.Core/DominoGame.cs
@@ -1,8 +1,8 @@
 using System;
 using Domino.Core.Objects;
+using Domino.Core.Systems;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace Domino.Core
 {
@@ -15,6 +15,7 @@ namespace Domino.Core
         public Texture2D Atlas { get; set; }        
         private Tile _selectedTile;
         
+        public InputManager Input { get; set; }
         public Random Random { get; } = new();
         public int Score { get; set; }
         public bool IsGameOver { get; set; }
@@ -44,57 +45,18 @@ namespace Domino.Core
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             Atlas = Content.Load<Texture2D>("Sprites/dominoes");
             Table = new Table(Atlas);
+            Input = new InputManager(table: Table);
+            _selectedTile = null;
             base.LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
-            var mouseState = Mouse.GetState();
-            Vector2 mousePosition = new Vector2(mouseState.X, mouseState.Y);
-
-            if (mouseState.LeftButton == ButtonState.Pressed)
-            {
-                if (_selectedTile == null)
-                {
-                    for (int i = Table.Tiles.Count - 1; i >= 0; i--)
-                    {
-                        if (Table.Tiles[i].Bounds.Contains(mousePosition))
-                        {
-                            _selectedTile = Table.Tiles[i];
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    _selectedTile.Scale = 1.5f;
-                    Vector2 targetPos = mousePosition - new Vector2(
-                        _selectedTile.SourceRectangle.Width / 2f, 
-                        _selectedTile.SourceRectangle.Height / 2f);
-                    
-                    _selectedTile.Position = Vector2.Lerp(
-                        _selectedTile.Position, targetPos, 0.4f);
-                    
-                    Vector2 delta = _selectedTile.Position - 
-                                    _selectedTile.LastPosition;
-                    
-                    float targetRotation = delta.X * 0.04f;
-                    _selectedTile.Rotation = MathHelper.Lerp(
-                        _selectedTile.Rotation, 
-                        targetRotation, 0.5f);
-                    
-                    _selectedTile.LastPosition = _selectedTile.Position;
-                }
-            }
-            else
-            {
-                _selectedTile = null;
-                foreach(var t in Table.Tiles) {
-                    t.Rotation = MathHelper.Lerp(t.Rotation, 0, 0.1f);
-                    t.Scale = MathHelper.Lerp(t.Scale, 1, 0.2f);
-                }
-            }
-
+            Input.Update(
+                game: this,
+                selectedTile: ref _selectedTile,
+                gameTime: gameTime);
+            
             base.Update(gameTime);
         }
 
